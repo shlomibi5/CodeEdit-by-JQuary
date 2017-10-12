43ac1a3b708c5544c9311508af402cec41e872d6
shlomi bitan <shlomibi5@gmail.com>
2017-10-13 00:24:05 +0300

    part1

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+
+      /*  $("#htmltext").click(function(){
+          //  var panelid = $(this).attr("id") + "panel1";
+           // $("#" + panelid).toggle();
+           alert("afafaf");
+        });
+
+        $(".panel1").height($("windows").height() - $("#header").height() - 15);
+        $(".panel1").width(($("windows").width() / 2) - 10);
+
+        $("#htmltext").on('change keyup paste', function(){
+            $("#outputscreen").contents().find("html").html($("#htmltext").val());
+        });
+
+        */
